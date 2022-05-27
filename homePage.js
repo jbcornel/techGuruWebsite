10ab8f2f0e8d24d756210736961eedfa09969dea
Joseph Cornell <59591937+jbcornel@users.noreply.github.com>
2022-05-27 13:54:58 -0400

    new 3d model with associated three js code

--- homePage.js
+++ homePage.js
@@ -3,157 +3,169 @@ import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/t
 //import {VRButton} from 'https://cdn.jsdelivr.net/npm/three@0.134.0/examples/jsm/webxr/VRButton.min.js';
 import {FBXLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/FBXLoader.js';
 import {GLTFLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js';
-//import { GUI } from 'dat.gui'
+
+
 const scene = new THREE.Scene();
 
+
+
 //scene.background = new THREE.Color( 0xf0f0ff );
+ 
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000)
 
 
-//adding plane helper 
-// const plane = new THREE.Plane( new THREE.Vector3( 1, 1, 0.2 ), 3 );
-// const helper = new THREE.PlaneHelper( plane, 1, 0xffff00 );
-// scene.add( helper );
+camera.position.x = 300;
+camera.position.y = 300;
+camera.position.z = 300;
 
-
-//adding light helper into scene
-// const pointLight = new THREE.PointLight( 0xff0000, 1, 100 );
-// pointLight.position.set( 10, 10, 10 );
-// scene.add( pointLight );
-
-// const sphereSize = 1;
-// const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
-// scene.add( pointLightHelper );
-
-
-
-
-
-
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.setZ(.3);
-camera.position.setX(0);
-camera.position.setY(.15);
-//camera settings for red house 3d model
-// camera.position.setZ(16);
-// camera.position.setY(5);
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg'),alpha: true, antialias: true
+    canvas: document.querySelector('#bg'),alpha: true,
 });
-renderer.shadowMap.enabled = true;
-document.body.appendChild(renderer.domElement);
-
-// WINDOW RESIZE HANDLING
-export function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-window.addEventListener('resize', onWindowResize);
-
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight)
 //renderer.render(scene,camera);
 //renderer.xr.enabled = true;
+const orbit = new OrbitControls(camera, renderer.domElement);
+document.body.appendChild(renderer.domElement)
 
 
-let light = new THREE.DirectionalLight(0xFFFFFF, 2.0);
-light.position.set(-100, 100, 50);
-let light2 = new THREE.DirectionalLight(0xFFFFFF, 2.0);
-light2.position.set(-100, 100, -50);
-light2.target.position.set(0, 0, 0);
-light.target.position.set(0, 0, 0);
-light.castShadow = true;
-light.shadow.bias = -0.001;
-light.shadow.mapSize.width = 4096;
-light.shadow.mapSize.height = 4096;
-light.shadow.camera.near = 0.1;
-light.shadow.camera.far = 500.0;
-light.shadow.camera.near = 0.5;
-light.shadow.camera.far = 500.0;
-light.shadow.camera.left = 50;
-light.shadow.camera.right = -50;
-light.shadow.camera.top = 50;
-light.shadow.camera.bottom = -50;
+
+const sphereSize = 1;
+
+
+const width = 10;
+const height = 10;
+const intensity = 50000;
+const rectLight = new THREE.RectAreaLight( 0xEFD31A, intensity*.9,  width, height );
+rectLight.position.set( 500, -500, 450 );
+rectLight.lookAt( 0, 100, 0 );
+scene.add( rectLight )
+
+
+const rectLight2 = new THREE.RectAreaLight( 0xB63FFF, intensity*.1,  width, height );
+rectLight2.position.set( -200, 200, 175 );
+rectLight2.lookAt( 0, 100, 0 );
+scene.add( rectLight2 )
+
+
+const rectLight3 = new THREE.RectAreaLight( 0xB6aFFF, intensity*.3,  width, height );
+rectLight3.position.set( 200, 200, -250 );
+rectLight3.lookAt( 0, 100, 0 );
+scene.add( rectLight3 )
+
+const rectLight4 = new THREE.RectAreaLight( 0xB6aFFF, intensity*.1,  width, height );
+rectLight4.position.set( -230, 200, -250 );
+rectLight4.lookAt( 0, 100, 0 );
+scene.add( rectLight4 )
+//x is up
+//y is backward
+//z is left
+const rectLight7 = new THREE.RectAreaLight( 0xEFD31A, intensity*.2,  width, height );
+rectLight7.position.set( 200,220,-20 );
+rectLight7.lookAt( 0, 200, 0 );
+scene.add( rectLight7 )
+
+
+
+
+const phoneLight = new THREE.RectAreaLight( 0xFFAE42, intensity*.01,  width, height );
+phoneLight.position.set( 0,150, 200 );
+phoneLight.lookAt( 0,0,0);
+phoneLight.castShadow = true;
+scene.add( phoneLight )
+
+//
+
+
+let light = new THREE.AmbientLight(0xFFFFFF, 0.15);
 scene.add(light);
-scene.add(light2);
 
-light = new THREE.AmbientLight(0xFFFFFF, 0.25);
-scene.add(light);
-const geometry2 = new THREE.BoxGeometry(5, 5, 5 ,);
-const material2 = new THREE.MeshStandardMaterial({
-    color: 0x393939,
-
-});
-const cube = new THREE.Mesh(geometry2, material2);
-//scene.add(cube);
 
 //document.body.appendChild( VRButton.createButton( renderer ) );
+// scene.background = new THREE.Color(0xBFDCE8);
+let mixer;
 
 
 
-scene.background = new THREE.Color(0x393939);
-const loader = new GLTFLoader();
+const progressBar =  document.getElementById('progressBar'); 
+const progressBarContainer =  document.getElementById('progressBarContainer'); 
+const loadingManager = new THREE.LoadingManager();
+let sceneObj;
+loadingManager.onProgress = function(url, loaded, total) {
+    progressBar.value = (loaded / total) * 100;
+}
 
-      loader.setPath('./resources/brokenPhone/');
-      loader.load('scene.gltf', (gltf) => {
+loadingManager.onLoad = function() {
+    progressBarContainer.style.display = 'none';
+}
+const loader = new GLTFLoader(loadingManager);
+
+      loader.setPath('./resources/techGuruModel/');
+      loader.load('techguruWebModel.glb', (gltf) => {
        
         let sceneInput = gltf.scene;
+     
+
+
+        //part of code that loads the animation of the 3d model
         
-        // GUI.add(gltf.scene.rotation, 'x').min(0).max(9);
-        // GUI.add(gltf.scene.rotation, 'y').min(0).max(9);
-        // GUI.add(gltf.scene.rotation, 'z').min(0).max(9);
+
        
+      
         scene.add(sceneInput);
-        scene.position.setX(-.08)
-        
         renderer.render(scene, camera);
-       })     
-       
-    //    loader.setPath('./resources/table/');
-    //   loader.load('table.gltf', (gltf) => {
-       
-    //     let sceneInput = gltf.scene;
+        mixer = new THREE.AnimationMixer(sceneInput);
+    const clips = gltf.animations;
+
+
+
     
+   // Play a certain animation
+    // const clip = THREE.AnimationClip.findByName(clips, 'addressAnimation');
+    // const action = mixer.clipAction(clip);
+    // action.play();
+
+    // Play all animations at the same time
+   
+        
+        clips.forEach(function(clip) {
+            const action = mixer.clipAction(clip);
+            action.play();
+        });
+       })      
+
+
+
+
       
-    //     scene.add(sceneInput);
-        
-    //     renderer.render(scene, camera);
-
-        
-    //    })     
-
        
+       renderer.outputEncoding = THREE.sRGBEncoding;
 
 
-      
-       
-        renderer.outputEncoding = THREE.sRGBEncoding;
+      //  const fbxLoader = new FBXLoader();
 
-
-    //    const fbxLoader = new FBXLoader();
-
-    //    fbxLoader.setPath('./resources/brokenPhone/');
-    //    fbxLoader.load('cellphone.fbx', (fbx) => {
+      //  fbxLoader.setPath('./resources/lowPoly/');
+      //  fbxLoader.load('phone.fbx', (fbx) => {
         
         
-    //     console.log(fbx);
+      //   console.log(fbx);
          
        
-    //      scene.add(fbx);
-    //      renderer.render(scene, camera);
-    //     })      
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.update();
+      //    //scene.add(fbx);
+      //    renderer.render(scene, camera);
+      //   })      
+// const controls = new OrbitControls( camera, renderer.domElement );
+// controls.update();
 
 
 
 //setInterval(changeBackground(), 1000)
-
+const clock = new THREE.Clock();
 function animate() {
-    
 	requestAnimationFrame( animate );
-    controls.update();
+    orbit.update();
+    if(mixer)
+        mixer.update(clock.getDelta());
 
     //scene.background = null
 	renderer.render( scene, camera );
